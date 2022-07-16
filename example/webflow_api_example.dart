2566d0c7eba87970a34dfd45ef5ef18d757ad71e
ElBatanony <ahmedelbatanony@hotmail.com>
2022-07-16 15:59:58 +0300

    Initial package setup

---
+++ example/webflow_api_example.dart
@@ -0,0 +1,6 @@
+import 'package:webflow_api/webflow_api.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/webflow_api.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library webflow_api;
+
+export 'src/webflow_api_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
