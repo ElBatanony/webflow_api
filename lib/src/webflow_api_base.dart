032d8d126f3becb4fe2c9f51519ee00da31cf3cf
ElBatanony <ahmedelbatanony@hotmail.com>
2022-07-18 09:10:13 +0300

    Add base Webflow class

--- lib/src/webflow_api_base.dart
+++ lib/src/webflow_api_base.dart
@@ -4,3 +4,26 @@
 class Awesome {
   bool get isAwesome => true;
 }
+
+class Webflow {
+  String endpoint;
+  String token;
+  String version;
+  String siteId;
+  Map<String, String>? headers;
+
+  Webflow({
+    required this.token,
+    required this.siteId,
+    this.version = "1.0.0",
+    this.endpoint = "api.webflow.com",
+  }) {
+    headers = {
+      "Accept": "application/json",
+      "Authorization": "Bearer $token",
+      "accept-version": version,
+      "Content-Type": "application/json",
+    };
+  }
+
+}
